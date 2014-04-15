dc00055c2ef0afb78b3b15e7c9214668993facb0
Andrej Manduch <amanduch@gmail.com>
2014-04-15 18:14:54 +0200

    alchemy init

--- mysql_log.py
+++ mysql_log.py
@@ -1,34 +1,59 @@
-from pyhwole.core import plugin, utils
+from pyhole.core import plugin, utils
 from xlc_libs.message import message as bMessage
-
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column
+from sqlalchemy import Integer, String, Timestamp, MetaData
 
 class MysqlLogger(plugin.Plugin):
     def __init__(self, irc):
+#        super(plugin.Plugin, self).__init__(irc)
         self.irc = irc
         self.name = self.__class__.__name__
+#        self.disabled = False
         try:
             self.config = utils.get_config("MysqlLogger")
-            self.db_name = self.config.get("db_name")
-            self.db_user = self.config.get("db_user")
-            self.db_pass = self.config.get("db_password")
+            db_name = self.config.get("db_name")
+            db_user = self.config.get("db_user")
+            db_pass = self.config.get("db_password")
+            db_host = self.config.get("db_host")
             try:
-                self.db_type = self.config.get("db_type")
+                #FIXME: not implemented
+                db_type = self.config.get("db_type")
             except:
-                self.db_type = "mysql"
+                db_type = "mysql"
 
         except Exception:
             print "Could not load MysqlLogger"
-            self.disabled = True
+            #self.disabled = True
+        #DbConnect
+        #FIXME: add try, case block
+        self.db_engine = create_engine(
+            '' + db_type + '://' + db_user + ':' + db_pass + '@' + db_host +
+            '/' + db_name
+        )
+        self.db_metadata = MetaData()
+        self.db_chanlog = Table(
+            'chanlog', self.db_metadata,
+            Column('ID', Integer, primary_key=True),
+            Column('sender', String),
+            Column('sender_user', String),
+            Column('message', String),
+            Column('timestamp', Timestamp)
+        )
+        #FIXME: if table didn't exist call _installPlugin
 
-        @plugin.hook_add_command('ahoj')
-        def ahoj(self, message, params=None, **kwargs):
-            message.dispatch("no, cau")
+    def _installPlugin(self):
+        self.db_metadata.create_all(self.db_engine)
 
-        @plugin.hook_add_msg_regex('.*')
-        def mysql_logger(self, message, params=None, **kwargs):
-            print "hook zareagoval"
-            msg = bMessage(message)
-            print "from:", msg.getSender(), "EOL"
-            print "to:", msg.getRecipient(), "EOL"
-            print "channel:", msg.getChannel(), "EOL"
-            print "text:", msg.getText(), "EOL"
+    @plugin.hook_add_command('ahoj')
+    def ahoj(self, message, params=None, **kwargs):
+        message.dispatch("no, cau")
+
+    @plugin.hook_add_msg_regex('.*')
+    def mysql_logger(self, message, params=None, **kwargs):
+        print "hook zareagoval"
+        msg = bMessage(message)
+        print "from:", msg.getSender(), "EOL"
+        print "to:", msg.getRecipient(), "EOL"
+        print "channel:", msg.getChannel(), "EOL"
+        print "text:", msg.getText(), "EOL"
