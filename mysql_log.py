778d7dcfa312457fb01625f7e39263f89ce333c2
Andrej Manduch <amanduch@gmail.com>
2014-04-15 15:04:08 +0200

    snapshot

--- mysql_log.py
+++ mysql_log.py
@@ -1,15 +1,29 @@
-from pyhole.core import plugin, utils
+from pyhwole.core import plugin, utils
 from xlc_libs.message import message as bMessage
 
+
 class MysqlLogger(plugin.Plugin):
-#    def __init__(self, irc):
-#        self.irc = irc
-#        self.name = self.__class__.__name__
-        
+    def __init__(self, irc):
+        self.irc = irc
+        self.name = self.__class__.__name__
+        try:
+            self.config = utils.get_config("MysqlLogger")
+            self.db_name = self.config.get("db_name")
+            self.db_user = self.config.get("db_user")
+            self.db_pass = self.config.get("db_password")
+            try:
+                self.db_type = self.config.get("db_type")
+            except:
+                self.db_type = "mysql"
+
+        except Exception:
+            print "Could not load MysqlLogger"
+            self.disabled = True
+
         @plugin.hook_add_command('ahoj')
         def ahoj(self, message, params=None, **kwargs):
             message.dispatch("no, cau")
-        
+
         @plugin.hook_add_msg_regex('.*')
         def mysql_logger(self, message, params=None, **kwargs):
             print "hook zareagoval"
--- xlc_libs/message.py
+++ xlc_libs/message.py
@@ -1,5 +1,5 @@
-from pyhole.core import plugin, utils
 import re
+
 
 class message(object):
     def __init__(self, message):
@@ -9,26 +9,35 @@ class message(object):
         self._parse_msg()
         self._fullIdent = message.source
         self._parse_nick()
-        
+
     def getChannel(self):
         reObj = re.match(r'([^\s]*).*', self.channel)
         try:
             return reObj.group(1)
         except:
             return ""
-        
+
+    def getSenderIdent(self):
+        return self._senderIdent
+
     def getSender(self):
         return self.sender
-    
+
     def getRecipient(self):
         return self.recipient
-        
+
     def getText(self):
         return self.text
-        
+
+    def getFullText(self):
+        return self._fullMessage
+
     def _parse_msg(self):
         #FIXME: unit test needed
-        reObj = re.match(r'[\s]*([0-9A-Za-z_-~]{0,9})[\s]*:[\s]*(.*)', self._fullMessage)
+        reObj = re.match(
+            r'[\s]*([0-9A-Za-z_-~]{0,9})[\s]*:[\s]*(.*)',
+            self._fullMessage
+            )
         if reObj:
             self.recipient = reObj.group(1)
             self.text = reObj.group(2)
@@ -37,5 +46,7 @@ class message(object):
             self.text = self._fullMessage
 
     def _parse_nick(self):
-        reObj = re.match(r'(.*)!.*', self._fullIdent) #FIXME: what if is \! in identity
+        #FIXME: what if is \! in identity
+        reObj = re.match(r'(.*)!(.*)', self._fullIdent)
         self.sender = reObj.group(1)
+        self._senderIdent = reObj.group(2)
